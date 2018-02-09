6397880d8d080f4b089b56d6e262c488359edf83
Phil Smith <phil@amillionmonkeys.co.uk>
2018-02-09 10:29:28 +0000

    fixes

--- admin/config/config.production.php
+++ admin/config/config.production.php
@@ -1,1 +1,14 @@
 <?php
+
+	define('PERCH_SITEPATH', '/Users/philsmith/code/purple-door');
+
+	define('PERCH_SCHEDULE_SECRET', 'QtL2QvCzWjgPlzY3');
+
+	define('PERCH_DB_USERNAME', 'stagings_purple');
+	define('PERCH_DB_PASSWORD', '%2X2;e*zYKD$');
+	define('PERCH_DB_SERVER', 	"localhost");
+	define('PERCH_DB_DATABASE', 'stagings_purple');
+	define('PERCH_DB_PREFIX', 	'perch3_');
+	define('PERCH_DEBUG', 	false);
+	
+
