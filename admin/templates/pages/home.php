bd8fff0c0d61504b9f522303f8885041d3b788c8
Phil Smith <phil@amillionmonkeys.co.uk>
2018-02-13 12:46:35 +0000

    Reduce featured

--- admin/templates/pages/home.php
+++ admin/templates/pages/home.php
@@ -6,6 +6,8 @@
     <?php perch_content('Hero'); ?>
 
     <?php perch_collection('projects', [
+        'filter'=>'featured',
+        'value'=>1
     ]); ?>
 </main>
 <?php perch_layout('footer'); ?>
